8fed0907a814037037dce2b513291b7b7ffadb9b
Lom Emeka Collins <lomemeka10@gmail.com>
2026-08-31 16:19:06 +0000

    feat: detect lowercase letters

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ password = input("Enter your password: ")
 
 length = len(password)
 has_uppercase = any(char.isupper() for char in password)
+has_lowercase = any(char.islower() for char in password)
 
 print("Password length:", length)
 print("Contains uppercase:", has_uppercase)
+print("Contains lowercase:", has_lowercase)
