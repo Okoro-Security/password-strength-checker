8b11676dfb4791ad9f30982ef129278c09e3e14e
Lom Emeka Collins <lomemeka10@gmail.com>
2026-08-31 17:12:38 +0000

    refactor: create password checking function

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
+def check_password(password):
+    length = len(password)
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
+
+    score = 0
+
+    if length >= 8:
+        score += 1
+
+    if has_uppercase:
+        score += 1
+
+    if has_lowercase:
+        score += 1
+
+    if has_digit:
+        score += 1
+
+    if has_special:
+        score += 1
+
+    if score <= 2:
+        strength = "Weak"
+    elif score <= 4:
+        strength = "Medium"
+    else:
+        strength = "Strong"
+
+    return score, strength
+
+
 password = input("Enter your password: ")
 
-length = len(password)
-has_uppercase = any(char.isupper() for char in password)
-has_lowercase = any(char.islower() for char in password)
-has_digit = any(char.isdigit() for char in password)
-has_special = any(not char.isalnum() for char in password)
+score, strength = check_password(password)
 
-score = 0
-
-if length >= 8:
-    score += 1
-
-if has_uppercase:
-    score += 1
-
-if has_lowercase:
-    score += 1
-
-if has_digit:
-    score += 1
-
-if has_special:
-    score += 1
-
-if score <= 2:
-    strength = "Weak"
-elif score <= 4:
-    strength = "Medium"
-else:
-    strength = "Strong"
-
-print("Password length:", length)
-print("Contains uppercase:", has_uppercase)
-print("Contains lowercase:", has_lowercase)
-print("Contains digit:", has_digit)
-print("Contains special character:", has_special)
 print("Password score:", score, "/ 5")
 print("Password strength:", strength)
 
