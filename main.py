bd4ec521062ca58383f5641cca9a04f549552aac
Lom Emeka Collins <lomemeka10@gmail.com>
2026-08-31 16:10:41 +0000

    feat: detect uppercase letters

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 password = input("Enter your password: ")
 
 length = len(password)
+has_uppercase = any(char.isupper() for char in password)
 
 print("Password length:", length)
+print("Contains uppercase:", has_uppercase)
